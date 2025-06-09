97d581906b6b945f80dea5da2ff93bf71c29823c
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-09 14:12:13 -0300

    feat: implements cbos module

---
+++ src/app/(private)/empresas/cbos/components/data-table/columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "./data-table-column-header";
+import { DataTableRowActions } from "./data-table-row-actions";
+import { DepartmentObjectResponse } from "../../interfaces/cbo-interface";
+
+export const columns: ColumnDef<DepartmentObjectResponse>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ID" />
+    ),
+    cell: ({ row }) => <div className="text-nowrap">{row.getValue("id")}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "nome",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Nome" />
+    ),
+    cell: ({ row }) => (
+      <span className="truncate font-medium">{row.getValue("nome")}</span>
+    ),
+  },
+  {
+    accessorKey: "criadoPor",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Criado por" />
+    ),
+    cell: ({ row }) => <span>{row.getValue("criadoPor")}</span>,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <DataTableRowActions row={row} />,
+  },
+];
