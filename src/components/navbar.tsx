7d0fe2719b5c72411da940f1cf808f62437c4001
puro9160@@ <paulo.henriqueicm2011@gmail.com>
2025-06-10 08:29:50 -0300

    fix(company-crud): removing submenu and fixing id rowactions column

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar() {
   const user = session?.user;
 
   const [activeMenu, setActiveMenu] = useState<string | null>(null);
-  const [activeSubmenu, setActiveSubmenu] = useState<string | null>(null);
+  const [activeSubmenu] = useState<string | null>(null);
   const [isMounted, setIsMounted] = useState(false);
 
   // Evita renderizar até que o componente esteja montado no cliente
@@ -131,8 +131,7 @@ export default function Navbar() {
     );
     setActiveMenu(menu?.id || null);
 
-    const submenu = menu?.submenu?.find((sub) => pathname === sub.path);
-    setActiveSubmenu(submenu?.id || null);
+
   }, [pathname, isMounted]);
 
   const logout = async () => {
