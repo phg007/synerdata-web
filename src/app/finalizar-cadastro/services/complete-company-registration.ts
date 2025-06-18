aa74f18e245c4a3f00e11d32ca5150d1544312ee
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-18 09:20:54 -0300

    feat: adds page to complete company registry in first login

---
+++ src/app/finalizar-cadastro/services/complete-company-registration.ts
@@ -0,0 +1,55 @@
+import { fetchClient } from "@/utils/fetch-client";
+import { ApiResponse } from "@/utils/interfaces/base-response";
+
+export interface CompleteCompanyRegistrationPayload {
+  dataFundacao: string;
+  faturamento: number;
+  regimeTributario: string;
+  inscricaoEstadual: string;
+  cnaePrincipal: string;
+  segmento: string;
+  ramoAtuacao: string;
+  empresaId: string;
+}
+
+export async function completeCompanyRegistration({
+  dataFundacao,
+  faturamento,
+  regimeTributario,
+  inscricaoEstadual,
+  cnaePrincipal,
+  segmento,
+  ramoAtuacao,
+  empresaId,
+}: CompleteCompanyRegistrationPayload) {
+  try {
+    const response = await fetchClient(
+      `v1/empresas/${empresaId}/finalizar-cadastro`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          dataFundacao,
+          faturamento,
+          regimeTributario,
+          inscricaoEstadual,
+          cnaePrincipal,
+          segmento,
+          ramoAtuacao,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+
+      throw new Error(
+        errorData.error.message || "Ocorreu um erro criar a filial."
+      );
+    }
+
+    return (await response.json()) as ApiResponse<null>;
+  } catch (error) {
+    console.error("Ocorreu um erro ao criar a filial.", error);
+    throw error;
+  }
+}
