ae16442c8b55a55d5e3b7f3fd2107696bbb3ccd8
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2025-06-18 10:04:24 -0300

    fix: adjusts next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
